0162825a5c5f32da4ecd326bbb2c356d5c667991
samir khan <30944973+samir1234khans@users.noreply.github.com>
2026-08-27 08:28:41 +0530

    style: format the data package public API

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -13,11 +13,9 @@ export type {
 import type { Database } from "./database.types";
 
 export type AccountRow = Database["private"]["Tables"]["accounts"]["Row"];
-export type ContributorProfileRow =
-  Database["public"]["Tables"]["contributor_profiles"]["Row"];
+export type ContributorProfileRow = Database["public"]["Tables"]["contributor_profiles"]["Row"];
 export type ContributorProfileInsert =
   Database["public"]["Tables"]["contributor_profiles"]["Insert"];
 export type ContributorProfileUpdate =
   Database["public"]["Tables"]["contributor_profiles"]["Update"];
-export type PublicProfileDirectoryRow =
-  Database["public"]["Views"]["profile_directory"]["Row"];
+export type PublicProfileDirectoryRow = Database["public"]["Views"]["profile_directory"]["Row"];
